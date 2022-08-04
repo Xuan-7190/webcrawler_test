8f73d8d951bf7ca12092114735050b5a0e5518e1
Xuan-7190 <59555748+Xuan-7190@users.noreply.github.com>
2022-08-04 11:22:58 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from app_function import get_save_data_top30_json, get_save_data_df, get_save_data_json, search_numbers_combination, search_numbers_combination_limited
+from app_function import get_data_length, get_save_data_top30_json, get_save_data_df, get_save_data_json, search_numbers_combination
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False # 讓傳出去的json可以中文顯示
+app.config['JSON_SORT_KEYS'] = False # 輸出的json資料不要讓他排序
+
 @app.route('/get_save_data_top30')
 def get_save_data_top30_api():
     return get_save_data_top30_json()
@@ -12,10 +14,9 @@ def get_save_data_top30_api():
 def get_save_data_api():
     return get_save_data_json()
 
-@app.route('/search_numbers_combination/search_numbers=<search_numbers>')
-def search_numbers_combination_api(search_numbers):
-    return search_numbers_combination(search_numbers, get_save_data_df())
-
-@app.route('/search_numbers_combination_limited/period=<period>&search_numbers=<search_numbers>')
-def search_numbers_combination_limited_api(period, search_numbers):
-    return search_numbers_combination_limited(period, search_numbers, get_save_data_df())
+@app.route('/search_numbers_combination/search_numbers=<search_numbers>', defaults={'period': get_data_length(), 'next': 1})
+@app.route('/search_numbers_combination/search_numbers=<search_numbers>&period=<period>', defaults={'next': 1})
+@app.route('/search_numbers_combination/search_numbers=<search_numbers>&next=<next>', defaults={'period': get_data_length()})
+@app.route('/search_numbers_combination/search_numbers=<search_numbers>&period=<period>&next=<next>')
+def search_numbers_combination_api(search_numbers, period, next):
+    return search_numbers_combination(search_numbers, period, next, get_save_data_df())
