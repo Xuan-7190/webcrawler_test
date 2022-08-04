729c90fe15085fc875ccb04f380535531b66e184
Xuan-7190 <zx1999917@gmail.com>
2022-08-04 11:30:42 +0800

    test2

--- app_function.py
+++ app_function.py
@@ -7,7 +7,7 @@ from flask import jsonify
 # 抓近30期的資料(回傳json格式)
 def get_save_data_top30_json():
     df = pd.read_csv('./539樂透資料.csv')
-    df_top30 = df[:30]
+    df_top30 = df[:300]
     jdata = df_top30.to_json(orient='records', force_ascii=False)
     return jsonify(json.loads(jdata))
 
@@ -16,6 +16,13 @@ def get_save_data_top30_json():
 def get_save_data_df():
     df = pd.read_csv('./539樂透資料.csv')
     return df
+
+
+# 取得資料的全部範圍
+def get_data_length():
+    df = pd.read_csv('./539樂透資料.csv')
+    df_len = len(df)
+    return df_len
 
 
 # 抓全部的資料(回傳json格式)
@@ -42,41 +49,9 @@ def stringList_to_intList(input_list):
         output_list.append(int(i))
     return output_list
 
-
-# 搜尋組合從所有的資料中
-def search_numbers_combination(search_numbers, lotto_all_data_df_tmp):
-    try:
-        source_list = stringList_to_intList(search_numbers.split())
         
-        lotto_all_data_df_tmp['CK'] = False
-        lotto_all_data_df_tmp['NEXT'] = False
-        for i in range(len(lotto_all_data_df_tmp)):
-            target_list = stringList_to_intList(string_process(lotto_all_data_df_tmp['今彩539中獎號碼'][i]))     
-
-            # 判斷輸入的組合 有出現在這期 CK(check)=True
-            check_count = 0
-            for j in source_list:
-                if j in target_list:
-                    check_count+=1
-            if check_count == len(source_list):
-                lotto_all_data_df_tmp['CK'][i] = True
-                lotto_all_data_df_tmp['CK'][i-1] = True
-                lotto_all_data_df_tmp['NEXT'][i-1] = True
-
-        # 不需要回傳 CK 欄位
-        key_list = ['期數', '開獎日期', '今彩539中獎號碼', '備註']
-        lotto_all_data_df = lotto_all_data_df_tmp[lotto_all_data_df_tmp['CK']==True]
-        
-        jdata = lotto_all_data_df[key_list].to_json(orient='records', force_ascii=False)
-        return jsonify(json.loads(jdata))
-    
-    except Exception as e:
-        print(e)
-        print('輸入格式錯誤')
-
-
-# 搜尋組合從限制期數的資料中
-def search_numbers_combination_limited(period, search_numbers, lotto_all_data_df):
+# 搜尋組合從限制期數的資料中及要顯示下幾期
+def search_numbers_combination(search_numbers, period, next, lotto_all_data_df):
     try:
         lotto_all_data_df_tmp = lotto_all_data_df[:int(period)]
         
@@ -94,8 +69,8 @@ def search_numbers_combination_limited(period, search_numbers, lotto_all_data_df
                     check_count+=1
             if check_count == len(source_list):
                 lotto_all_data_df_tmp['CK'][i] = True
-                lotto_all_data_df_tmp['CK'][i-1] = True
-                lotto_all_data_df_tmp['NEXT'][i-1] = True
+                lotto_all_data_df_tmp['CK'][i-int(next)] = True
+                lotto_all_data_df_tmp['NEXT'][i-int(next)] = True
 
         # 不需要回傳 CK 欄位
         key_list = ['期數', '開獎日期', '今彩539中獎號碼', '備註']
