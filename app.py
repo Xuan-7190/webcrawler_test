c54a74208c9e17de13e9350c01e193dcdc134e82
Xuan-7190 <zx1999917@gmail.com>
2022-08-08 17:08:59 +0800

    update index and result (can search number)

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+import requests
+import json
+from flask import Flask, request, render_template, redirect
 from app_function import get_data_length, get_save_data_top30_json, get_save_data_df, get_save_data_json, search_numbers_combination
 
+url = 'https://flask-lto-app.herokuapp.com/search_numbers_combination/search_numbers='
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False # 讓傳出去的json可以中文顯示
 app.config['JSON_SORT_KEYS'] = False # 輸出的json資料不要讓他排序
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def post_submit():
+    # 取得輸入的數字組合
+    number = request.form.get('number')
+    # 串接url
+    search_url = url+number
+    # 呼叫api
+    response = requests.get(search_url)
+    print(type(response))
+    # 將結果
+    response_dic = response.json()
+    print(type(response_dic))
+    # print(response_dic['jdata'])
+    print(response_dic['jdata'][0]['今彩539中獎號碼'])
+    print(type(response_dic['jdata'][0]['今彩539中獎號碼']))
+    # test_result = json.loads(str(response_dic['jdata'][0]))
+    # print(test_result)
+    
+    return render_template('result.html', data=response_dic)
 
 @app.route('/get_save_data_top30')
 def get_save_data_top30_api():
